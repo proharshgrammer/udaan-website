0e4f2e9dfb4da5ce62d874b7fe6923b383fdca01
Harsh Sharma <coding.harsh2604@gmail.com>
2026-03-24 01:03:02 +0530

    Fix iOS Issues (Eruda & build target)

--- udaan-client/vite.config.js
+++ udaan-client/vite.config.js
@@ -4,6 +4,9 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2015'
+  },
   plugins: [
     react(),
     legacy({
